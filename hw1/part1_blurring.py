3aaf4e990b2452ca9f50214acb4b0c9dfa280a7a
yuvalgos <yuvalgos@gmail.com>
2021-12-07 15:03:29 +0200

    Blurred images

--- hw1/part1_blurring.py
+++ hw1/part1_blurring.py
@@ -1,7 +1,8 @@
-import numpy as np
 import scipy.io
+import scipy.signal
+import cv2
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 def load_trajectories(filename):
     """
@@ -48,18 +49,32 @@ def create_psf(trajectories, psf_size=20):
                                    trajectories[k][1],
                                    range=[[-10, 10], [-10, 10]],
                                    bins=psf_size,
-                                   cmap='gray')
-        plt.savefig(f"PSFs/PSF_{k}.png")
-        scipy.io.savemat(f"PSFs/PSF_{k}.mat", {'items': hist})
+                                   cmap='gray',
+                                   density=True)
+        # plt.savefig(f"PSFs/PSF_{k}.png")
+        # scipy.io.savemat(f"PSFs/PSF_{k}.mat", {'items': hist})
         psf_list.append(hist)
 
     return psf_list
+
+
+def motion_blur(img, psf_list):
+    """
+    Apply blur using all the PSF's and save to file
+    """
+    for k in range(len(psf_list)):
+        blurred_img = scipy.signal.convolve2d(img, psf_list[k], mode='same', boundary='wrap')
+        cv2.imwrite(f"blurred/blurred_{k}.png", blurred_img)
+
 
 trajectories = load_trajectories('100_motion_paths.mat')
 plot_trajectories(trajectories)
 
 psf_list = create_psf(trajectories, psf_size=20)
 
+im = cv2.imread("DIPSourceHW1.jpg", cv2.IMREAD_GRAYSCALE)
+
+motion_blur(im, psf_list)
 
 # if __name__ == '__main__':
 #     main()
