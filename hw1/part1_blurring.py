22d4c1414a0e824b0407896dc832161d013c47d2
yuvalgos <yuvalgos@gmail.com>
2021-12-07 12:24:30 +0200

    Added trajectories and PSF's

--- hw1/part1_blurring.py
+++ hw1/part1_blurring.py
@@ -22,7 +22,7 @@ def load_trajectories(filename):
 
 def plot_trajectories(trajectories):
     """
-    plot all trajectories in a single figure grid
+    plot all trajectories in a single figure grid and save to file
     """
     plt.figure(figsize=(10, 10))
     for i in range(len(trajectories)):
@@ -30,12 +30,36 @@ def plot_trajectories(trajectories):
         plt.plot(trajectories[i][0], trajectories[i][1])
         plt.xticks([])
         plt.yticks([])
+        plt.xlim([-10, 10])
+        plt.ylim([-10, 10])
 
+    plt.savefig('trajectories.png')
     plt.show()
 
 
+def create_psf(trajectories, psf_size=20):
+    """
+    Create PSF matrix for each trajectory, save it as an image and as matrix,
+    and return list of PSF's
+    """
+    psf_list = []
+    for k in range(len(trajectories)):
+        hist, _, _, _ = plt.hist2d(trajectories[k][0],
+                                   trajectories[k][1],
+                                   range=[[-10, 10], [-10, 10]],
+                                   bins=psf_size,
+                                   cmap='gray')
+        plt.savefig(f"PSFs/PSF_{k}.png")
+        scipy.io.savemat(f"PSFs/PSF_{k}.mat", {'items': hist})
+        psf_list.append(hist)
+
+    return psf_list
+
 trajectories = load_trajectories('100_motion_paths.mat')
 plot_trajectories(trajectories)
+
+psf_list = create_psf(trajectories, psf_size=20)
+
 
 # if __name__ == '__main__':
 #     main()
