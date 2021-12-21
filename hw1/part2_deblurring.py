7e3c3e06ef27a36b744a52b17ed779893f8ff150
yuvalgos <yuvalgos@gmail.com>
2021-12-21 20:12:39 +0200

    finished all requirements

--- hw1/part2_deblurring.py
+++ hw1/part2_deblurring.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import glob
 from scipy.fftpack import fft2, ifft2, fftshift, ifftshift
-
+import random
+import scipy.io
 
 def load_blurred_images():
     blurred_images = []
@@ -12,19 +13,20 @@ def load_blurred_images():
     for filename in filenames:
         im = cv2.imread(filename, cv2.IMREAD_GRAYSCALE) / 255.0
         blurred_images.append(im)
+        random.shuffle(blurred_images)
     return blurred_images
 
 
 def images_to_dft(images):
     blurred_images_fft = []
     for image in images:
-        blurred_images_fft.append(fftshift(fft2(image)))
+        blurred_images_fft.append(fftshift(fft2(image, shape=FFT_SIZE)))
     return blurred_images_fft
 
 
 def compute_fft_weights(images_fft, p=11):
     # smoothing filter
-    images_fft_smoothed = cv2.GaussianBlur(np.abs(images_fft), (5, 5), 0)
+    images_fft_smoothed = cv2.GaussianBlur(np.abs(images_fft), (11, 11), 1)
 
     fft_array_p = np.power(images_fft_smoothed, p)
 
@@ -45,42 +47,33 @@ def psnr(im1, im2):
     return 20 * np.log10(PIXEL_MAX) - 10 * np.log10(mse)
 
 
-# def get_deblurred_fft(curr_images_fft, p=11):
-#     deblurred_fft = np.zeros(curr_images_fft[0].shape, dtype=complex)
-#     total_weight = np.zeros_like(deblurred_fft)
-#     for image in curr_images_fft:
-#         weight = np.abs(image)
-#         weight = cv2.GaussianBlur(weight, (21, 21), 3)
-#         weight = weight ** p
-#
-#         deblurred_fft += weight * image
-#         total_weight += weight
-#
-#     return deblurred_fft / total_weight
-
-
 original_image = cv2.imread('DIPSourceHW1.jpg', cv2.IMREAD_GRAYSCALE) / 255.0
+image_size = original_image.shape
+FFT_SIZE = image_size
 
 blurred_images = load_blurred_images()
 blurred_images_fft = images_to_dft(blurred_images)
 blurred_images_fft = np.stack(blurred_images_fft)
 
 # reconstruct from different number of images:
-p = 13
-for num_images in range(1, 100):
+p = 9
+psnr_list = []
+for num_images in range(1, 101):
     curr_images_fft = blurred_images_fft[0:num_images]
 
     fft_weights = compute_fft_weights(curr_images_fft, p=p)
     reconstructed_fft = get_reconstructed_fft(curr_images_fft, fft_weights)
-    reconstructed_image = ifft2(ifftshift(reconstructed_fft)).real
+    reconstructed_image = ifft2(ifftshift(reconstructed_fft), shape=FFT_SIZE).real
 
-    print(f"num images = {num_images}   PSNR = {psnr(original_image, reconstructed_image)}")
+    cv2.imwrite(f"deblurred/{num_images}_images_deblur.jpg", reconstructed_image * 255)
 
-    plt.imshow(reconstructed_image, cmap='gray')
-    plt.title('num_images = ' + str(num_images))
-    plt.show()
+    psnr_list.append(psnr(original_image, reconstructed_image))
 
-    plt.imshow(np.log(np.abs(reconstructed_fft)))
-    plt.colorbar()
-    plt.title('num_images = ' + str(num_images))
-    plt.show()
+scipy.io.savemat('psnr_values.mat', mdict={'psnr': psnr_list})
+
+plt.plot(list(range(1, 101, 5)), psnr_list[::5])
+plt.title('PSNR vs. Number of Images')
+plt.xlabel('Number of Images')
+plt.ylabel('PSNR [dB]')
+plt.savefig('psnr_graph.png')
+plt.show()
